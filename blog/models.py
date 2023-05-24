8d6607bbff9ab01a40cfe02fa6f8826bf483b1a0
ajorgensen89 <ajorgensen89@gmail.com>
2023-05-24 16:49:35 +0000

    comment blog and fix error

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on-delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
