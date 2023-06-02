880da33b70e6d576c4fee480ea651bbfc8ded179
ajorgensen89 <ajorgensen89@gmail.com>
2023-06-02 12:58:36 +0000

    posts and comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     prepopulated_fileds = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    #action to approve comments. (more than one if needed)
+    actions = ['approve_comments']
+    # boolean result. set to true.
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
 
 
 # admin.site.register(Post)
