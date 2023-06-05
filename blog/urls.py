f1bf06048aa8a9dad7e09422654c00788a9f7628
ajorgensen89 <ajorgensen89@gmail.com>
2023-06-05 12:38:49 +0000

    like and unlike

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
 # path converter and slug random name
